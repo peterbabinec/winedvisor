74a04cf456db9db182efc0f45df0d060dec0a937
xbabinec <babinec.peter@gmail.com>
2017-06-28 13:28:04 +0200

    Add review feature added

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     # ex: /
     url(r'^$', views.review_list, name='review_list'),
     # ex: /reviews/5/
-    url(r'^reviews/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
     # ex: /wine/
     url(r'^wine$', views.wine_list, name='wine_list'),
     # ex: /wine/5/
     url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+    url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,10 @@
+import datetime
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Review
+from .forms import ReviewForm
 from .models import Wine
 
 
@@ -12,17 +16,37 @@ def review_list(request):
 
 def review_detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    return render(request, 'reviews/review_detail.html', {'reviews': review})
+    return render(request, 'reviews/review_detail.html', {'review': review})
 
 
 def wine_list(request):
-    wine_list = Wine.objects.order_by('-name')
+    wine_list = Wine.objects.order_by('-color')
     context = {'wine_list': wine_list}
     return render(request, 'reviews/wine_list.html', context)
 
 
 def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
+    form = ReviewForm()
     return render(request, 'reviews/wine_detail.html', {'wine': wine})
 
 
+def add_review(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        body = form.cleaned_data['body']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.wine = wine
+        review.user.name = user_name
+        review.rating = rating
+        review.body = body
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
+    return render(request, 'reviews/wine_detail.html', {'wine':wine, 'form':form})
