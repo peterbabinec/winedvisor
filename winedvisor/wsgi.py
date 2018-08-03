c010e59e9e9535b5e7b7b9a672fe5411817e53cd
Peter Babinec <peter.babinec@naytrolabs.com>
2018-08-03 12:49:38 +0200

    Modified wsgi

--- winedvisor/wsgi.py
+++ winedvisor/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for winedvisor project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For mor information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
